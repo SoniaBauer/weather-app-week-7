693757f4f6d6c6ee6b69c7dede3a23b30472b131
SoniaBauer <147061738+SoniaBauer@users.noreply.github.com>
2024-08-20 01:16:30 +0200

    added emoji, and fixed the positioning of the emoji a little in css.  made the  weather data functional

--- scr/index.js
+++ scr/index.js
@@ -7,12 +7,15 @@ function refreshWeather(response){
     let humidityElement=document.querySelector("#humidity");
     let windSpeedElement=document.querySelector("#wind-speed");
     let timeElement=document.querySelector("#time");
-    let date=new Date(response.data.time*1000);;
+    let date=new Date(response.data.time*1000);
+    let iconElement=documnet.querySelector("#icon");
+    
+     iconElement.innerHTML='<img src="${response.data.condition.icon_url}"  class="emoji"/>';
 
 
     cityElement.innerHTML=response.data.city;
     timeElement.innerHTML=formatDate[date];
-    descriptionElement.innerHTML=response.data.condtion.description;
+    descriptionElement.innerHTML=response.data.condition.description;
     humidityElement.innerHTML='${response.data.temperature.humidity}%';
     windSpeedElement.innerHTML='${response.data.wind.speed}km/h';
     temperatureElement.innerHTML= Math.round(temperature-numbers);
