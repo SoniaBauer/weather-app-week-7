9667e4235764936b0c1e7026e4508d89328d6dcc
SoniaBauer <147061738+SoniaBauer@users.noreply.github.com>
2024-08-20 00:16:31 +0200

    injected more weather data

--- scr/index.js
+++ scr/index.js
@@ -1,10 +1,50 @@
+
+function refreshWeather(response){
+    let temperatureElement= document.querySelector("#temperature-numbers");
+    let temperature=response.data.temperature.current;
+    let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#Descriptions");
+    let humidityElement=document.querySelector("#humidity");
+    let windSpeedElement=document.querySelector("#wind-speed");
+    let timeElement=document.querySelector("#time");
+    let date=new Date(response.data.time*1000);;
+
+
+    cityElement.innerHTML=response.data.city;
+    timeElement.innerHTML=formatDate[date];
+    descriptionElement.innerHTML=response.data.condtion.description;
+    humidityElement.innerHTML='${response.data.temperature.humidity}%';
+    windSpeedElement.innerHTML='${response.data.wind.speed}km/h';
+    temperatureElement.innerHTML= Math.round(temperature-numbers);
+
+}
+function formatDate(date){
+    
+    let minutes= date.getminutes();
+    let hours=date.gethours();
+    let days =["Monday","Tuesday", "Wednesday", "Thursday"," Friday","Saturday","Sunday"];
+    let day= days[date.getDay()];
+    return '${day} ${hours}:${minutes}';
+    if (minutes<10){
+        minutes='0${minutes}';
+    }
+}
+
+function searchCity(city){
+    let apiKey ="b5e07b0564et3ff29467o1feb88a0324";
+    let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+axios.get(apiUrl).then(refreshWeather);
+}
+
 function submitYourCity(event){
     event.preventDefault();
    let searchInput=document.querySelector("#input-search-form");
-   let cityElement=document.querySelector("#city");
-   cityElement.innerHTML=searchInput.value;
+   
+   searchCity(searchInput.value);
 }
 
 
 let searchFormElement=document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",submitYourCity);
+
+searchCity("Cape-Town");
