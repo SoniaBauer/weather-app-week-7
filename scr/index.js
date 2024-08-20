91779f0354dd3f8deaae6c7ef668b2ed4c6da79e
SoniaBauer <147061738+SoniaBauer@users.noreply.github.com>
2024-08-20 09:56:08 +0200

    added a new css and js and new index.html in

--- scr/index.js
+++ scr/index.js
@@ -1,53 +1,56 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature-numbers");
+    let temperature = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
 
-function refreshWeather(response){
-    let temperatureElement= document.querySelector("#temperature-numbers");
-    let temperature=response.data.temperature.current;
-    let cityElement=document.querySelector("#city");
-    let descriptionElement=document.querySelector("#Descriptions");
-    let humidityElement=document.querySelector("#humidity");
-    let windSpeedElement=document.querySelector("#wind-speed");
-    let timeElement=document.querySelector("#time");
-    let date=new Date(response.data.time*1000);
-    let iconElement=documnet.querySelector("#icon");
-    
-     iconElement.innerHTML='<img src="${response.data.condition.icon_url}"  class="emoji"/>';
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"  class="emoji"/>`;
 
-
-    cityElement.innerHTML=response.data.city;
-    timeElement.innerHTML=formatDate[date];
-    descriptionElement.innerHTML=response.data.condition.description;
-    humidityElement.innerHTML='${response.data.temperature.humidity}%';
-    windSpeedElement.innerHTML='${response.data.wind.speed}km/h';
-    temperatureElement.innerHTML= Math.round(temperature-numbers);
-
-}
-function formatDate(date){
-    
-    let minutes= date.getminutes();
-    let hours=date.gethours();
-    let days =["Monday","Tuesday", "Wednesday", "Thursday"," Friday","Saturday","Sunday"];
-    let day= days[date.getDay()];
-    return '${day} ${hours}:${minutes}';
-    if (minutes<10){
-        minutes='0${minutes}';
+    cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    temperatureElement.innerHTML = temperature;
+  }
+  function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      " Friday",
+      "Saturday",
+      "Sunday",
+    ];
+    let day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
     }
-}
+  }
 
-function searchCity(city){
-    let apiKey ="b5e07b0564et3ff29467o1feb88a0324";
-    let apiUrl ='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
-axios.get(apiUrl).then(refreshWeather);
-}
+  function searchCity(city) {
+    let apiKey = "b5e07b0564et3ff29467o1feb88a0324";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
 
-function submitYourCity(event){
+  function submitYourCity(event) {
     event.preventDefault();
-   let searchInput=document.querySelector("#input-search-form");
-   
-   searchCity(searchInput.value);
-}
+    let searchInput = document.querySelector("#input-search-form");
 
+    searchCity(searchInput.value);
+  }
 
-let searchFormElement=document.querySelector("#search-form");
-searchFormElement.addEventListener("submit",submitYourCity);
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", submitYourCity);
 
-searchCity("Cape-Town");
+  searchCity("Cape-Town");
