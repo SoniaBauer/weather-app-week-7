2836c522054b30d7cd759f1258b5f1e60c9c41bc
SoniaBauer <147061738+SoniaBauer@users.noreply.github.com>
2024-08-21 21:20:13 +0200

    added a new class with the weather forecast

--- scr/index.js
+++ scr/index.js
@@ -9,7 +9,7 @@ function refreshWeather(response) {
     let date = new Date(response.data.time * 1000);
     let iconElement = document.querySelector("#icon");
 
-    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"  class="emoji"/>`;
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"class="emoji"/>`;
 
     cityElement.innerHTML = response.data.city;
     timeElement.innerHTML = formatDate(date);
