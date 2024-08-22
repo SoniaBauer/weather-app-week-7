cd402244fc3149c51b431eefbb5268ebb88771b3
SoniaBauer <147061738+SoniaBauer@users.noreply.github.com>
2024-08-22 19:20:44 +0200

    inject the forecast from  Html from js

--- scr/index.js
+++ scr/index.js
@@ -53,4 +53,31 @@ function refreshWeather(response) {
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", submitYourCity);
 
-  searchCity("Cape-Town");
+  
+
+  function displayForecast(){
+let days = ["Mon","Tue","Wed","Thu"," Fri","Sat","Sun",];
+let forecastHtml="";
+
+    days.forEach(function (day){
+      forecastHtml =
+      forecastHtml +
+    `
+    <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">☀️</div>
+            <div class="weather-forecast-temperatures">
+            <div class="weather-forecast-temperature">
+               <strong>19°</strong>
+               </div>
+            <div class="weather-forecast-temperature"> 16°</div>
+            </div>
+            </div>
+            `;  
+  });
+
+  let forecastElement=document.querySelector("#forecast");
+  forecastElement.innerHTML= forecastHtml;
+}
+ searchCity("Cape-Town"); 
+ displayForecast();
